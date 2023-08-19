2311c5ee3982199100771a30cb7cff78cb9994cf
Suman16iitkgp <48875251+Suman16iitkgp@users.noreply.github.com>
2023-08-19 15:20:29 +0530

    2 files modified

--- src/app/housinglocation.ts
+++ src/app/housinglocation.ts
@@ -1,10 +1,10 @@
-export interface Housinglocation {
+export interface HousingLocation {
   id: number;
   name: String;
   city: String;
   state: String;
   photo: String;
-  availableUnits: String;
+  availableUnits: number;
   wifi: boolean;
   laundry: boolean;
 }
