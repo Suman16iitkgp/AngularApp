1ace6a2add20a5f608885f6a2d4015270ed5b4ad
Suman16iitkgp <48875251+Suman16iitkgp@users.noreply.github.com>
2023-08-20 14:15:00 +0530

    1 file modified, 1 added

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,18 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   template: `<p>
   housing-location works!
 </p>`,
   styleUrls: ['./housing-location.component.css'],
 })
-
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
